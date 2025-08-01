03a714bc8267b32923df27e7cb142c28764cf958
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-01 18:12:43 +0900

    Feat: Intimacy Compat

---
+++ Source/1.6/Mods/Intimacy/HarmonyInit.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace Maux36.RimPsyche.Intimacy
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            var harmony = new Harmony("Harmony_RimpsycheIntimacy");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Message($"[Rimpsyche] Intimacy patched");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[Rimpsyche] Intimacy patch failed: {e}");
+            }
+        }
+    }
+}
---
+++ Source/1.6/Mods/Intimacy/HarmonyPatches/IntimacyPatches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using LoveyDoveySexWithEuterpe;
+using Verse;
+
+namespace Maux36.RimPsyche.Intimacy
+{
+    [HarmonyPatch(typeof(InteractionWorker_StartConversation), nameof(InteractionWorker_StartConversation.InteractionHook))]
+    public class IntimacyPatches
+    {
+        private static void TryGainIntimacy(Pawn pawn, float amount)
+        {
+            (pawn.needs?.TryGetNeed<Need_Intimacy>())?.GainIntimacySocial(amount);
+        }
+
+        public static bool Prefix(Pawn initiator, Pawn recipient, float initOpinionOffset, float reciOpinionOffset)
+        {
+            if (initOpinionOffset > 0)
+            {
+                TryGainIntimacy(initiator, 0.01f);
+            }
+            if (reciOpinionOffset > 0)
+            {
+                TryGainIntimacy(recipient, 0.01f);
+            }
+            return true;
+
+        }
+    }
+}
