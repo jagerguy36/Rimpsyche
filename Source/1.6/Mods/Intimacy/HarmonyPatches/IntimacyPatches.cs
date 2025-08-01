00e427176a81b133cbd6b09553c1ba81967d07c8
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-01 21:50:39 +0900

    Feat: Intimacy patch

--- Source/1.6/Mods/Intimacy/HarmonyPatches/IntimacyPatches.cs
+++ Source/1.6/Mods/Intimacy/HarmonyPatches/IntimacyPatches.cs
@@ -1,11 +1,12 @@
 ﻿using HarmonyLib;
 using LoveyDoveySexWithEuterpe;
+using RimWorld;
 using Verse;
 
 namespace Maux36.RimPsyche.Intimacy
 {
     [HarmonyPatch(typeof(InteractionWorker_StartConversation), nameof(InteractionWorker_StartConversation.InteractionHook))]
-    public class IntimacyPatches
+    public class Intimacy_StartConversation_InteractionHookPatch
     {
         private static void TryGainIntimacy(Pawn pawn, float amount)
         {
@@ -26,4 +27,24 @@ namespace Maux36.RimPsyche.Intimacy
 
         }
     }
+
+    [HarmonyPatch(typeof(InteractionWorker), nameof(InteractionWorker.Interacted))]
+    public static class InteractionWorker_Interacted_Patch
+    {
+        private static void TryGainIntimacy(Pawn pawn, float amount)
+        {
+            (pawn.needs?.TryGetNeed<Need_Intimacy>())?.GainIntimacySocial(amount);
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(InteractionWorker __instance, Pawn initiator, Pawn recipient)
+        {
+            if (__instance != null && __instance.interaction?.defName != null && __instance.interaction.defName == "Rimpsyche_Smalltalk")
+            {
+                TryGainIntimacy(initiator, 0.01f);
+                TryGainIntimacy(recipient, 0.01f);
+            }
+        }
+    }
+
 }
