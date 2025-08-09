832e78d0dd21dc52c45fd91cba4ec6915398f559
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-09 17:20:48 +0900

    Feat: EdB Prepare Carefully show psyche button

--- Source/1.6/HarmonyPatches/EdB_PanelAppearance_Draw_Patch.cs
+++ Source/1.6/HarmonyPatches/EdB_PanelAppearance_Draw_Patch.cs
@@ -1,32 +1,32 @@
-//using HarmonyLib;
-//using System.Collections.Generic;
-//using System.Reflection;
-//using UnityEngine;
-//using Verse;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Verse;
 
 
-//namespace Maux36.RimPsyche
-//{
-//    [HarmonyPatch]
-//    public class EdB_PanelAppearance_Draw_Patch
-//    {
-//        public static bool Prepare()
-//        {
-//            return ModsConfig.IsActive("EdB.PrepareCarefully");
-//        }
+namespace Maux36.RimPsyche
+{
+    [HarmonyPatch]
+    public class EdB_PanelAppearance_Draw_Patch
+    {
+        public static bool Prepare()
+        {
+            return ModsConfig.IsActive("EdB.PrepareCarefully");
+        }
 
-//        public static IEnumerable<MethodBase> TargetMethods()
-//        {
-//            yield return AccessTools.Method("EdB.PrepareCarefully.PanelAppearance:Draw");
-//        }
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method("EdB.PrepareCarefully.PanelAppearance:Draw");
+        }
 
-//        public static void Postfix(Rect ___RectButtonRandomize, Pawn ___CachedPawn)
-//        {
-//            if (___CachedPawn?.compPsyche() != null)
-//            {
-//                Rect rect = new Rect(___RectButtonRandomize.x - ___RectButtonRandomize.width - 4f, ___RectButtonRandomize.y, ___RectButtonRandomize.width, ___RectButtonRandomize.height);
-//                Rimpsyche_UI_Utility.DrawEditButton(rect, ___CachedPawn);
-//            }
-//        }
-//    }
-//}
+        public static void Postfix(Rect ___RectButtonRandomize, Pawn ___CachedPawn)
+        {
+            if (___CachedPawn?.compPsyche() != null)
+            {
+                Rect rect = new Rect(___RectButtonRandomize.x - ___RectButtonRandomize.width - 4f, ___RectButtonRandomize.y, ___RectButtonRandomize.width, ___RectButtonRandomize.height);
+                Rimpsyche_UI_Utility.DrawEditButton(rect, ___CachedPawn);
+            }
+        }
+    }
+}
