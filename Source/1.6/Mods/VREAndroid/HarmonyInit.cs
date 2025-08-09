19d97d775691a0acff3d5172810266221406e803
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-09 17:21:01 +0900

    Feat: VRE Android Integration

---
+++ Source/1.6/Mods/VREAndroid/HarmonyInit.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace Maux36.RimPsyche.VREAndroid
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            var harmony = new Harmony("Harmony_RimpsycheVREAndroid");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Message($"[Rimpsyche] VREAndroid patched");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[Rimpsyche] VREAndroid patch failed: {e}");
+            }
+        }
+    }
+}
---
+++ Source/1.6/Mods/VREAndroid/HarmonyPatches.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Maux36.RimPsyche.VREAndroid
+{
+    [StaticConstructorOnStartup]
+    public class VRE_Android_TraitDatabaseProvider
+    {
+        static VRE_Android_TraitDatabaseProvider()
+        {
+            Log.Message("[Rimpsyche] VRE Android gate data added");
+            RimpsycheDatabase.GeneGateDatabase["VREA_PsychologyDisabled"] = new List<FacetGate>
+            {
+                new FacetGate(Facet.Imagination, 0f, 0f, 10),
+                new FacetGate(Facet.Intellect, 0f, 0f, 10),
+                new FacetGate(Facet.Curiosity, 0f, 0f, 10),
+                new FacetGate(Facet.Industriousness, 20f, 0f, 10),
+                new FacetGate(Facet.Orderliness, 0f, 0f, 10),
+                new FacetGate(Facet.Integrity, 20f, 0f, 10),
+                new FacetGate(Facet.Sociability, 20f, 0f, 10),
+                new FacetGate(Facet.Assertiveness, 0f, 0f, 10),
+                new FacetGate(Facet.Enthusiasm, 0f, 0f, 10),
+                new FacetGate(Facet.Compassion, 30f, 0f, 10),
+                new FacetGate(Facet.Cooperation, 30f, 0f, 10),
+                new FacetGate(Facet.Humbleness, 30f, 0f, 10),
+                new FacetGate(Facet.Volatility, -30f, 0f, 10),
+                new FacetGate(Facet.Pessimism, -30f, 0f, 10),
+                new FacetGate(Facet.Insecurity, 0f, 0f, 10),
+            };
+        }
+    }
+}
