dac840dcff374fdaeffa8d9c71189253c2f34a9c
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-15 09:01:17 +0900

    Fix: Safety

--- Source/1.6/HarmonyPatches/Pawn_InteractionTracker_fixes.cs
+++ Source/1.6/HarmonyPatches/Pawn_InteractionTracker_fixes.cs
@@ -13,7 +13,7 @@ namespace Maux36.RimPsyche
             if (__result > 0f)
             {
                 var compPsyche = ___pawn.compPsyche();
-                if (compPsyche.psycheEnabled == true)
+                if (compPsyche?.psycheEnabled == true)
                 {
                     __result = Mathf.Clamp01(__result * compPsyche.Personality.Evaluate(RimpsycheDatabase.SocialFightChanceMultiplier));
                 }
