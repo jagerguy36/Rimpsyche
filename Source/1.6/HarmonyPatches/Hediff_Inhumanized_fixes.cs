488631b7b9874be1c2884eb6bb80a5bc60443a33
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-08-18 18:56:37 +0900

    Feat: Inhumanize check implementation

---
+++ Source/1.6/HarmonyPatches/Hediff_Inhumanized_fixes.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Maux36.RimPsyche
+{
+    [HarmonyPatch(typeof(Hediff_Inhumanized), nameof(Hediff_Inhumanized.PostAdd))]
+    public static class Hediff_Inhumanized_PostAdd
+    {
+        public static void Postfix(Hediff_Inhumanized __instance, Pawn ___pawn)
+        {
+            var compPsyche = ___pawn.compPsyche();
+            if (compPsyche! == null)
+            {
+                compPsyche.NullifyCheck();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Hediff_Inhumanized), nameof(Hediff_Inhumanized.PostRemoved))]
+    public static class Hediff_Inhumanized_PostRemoved
+    {
+        public static void Postfix(Hediff_Inhumanized __instance, Pawn ___pawn)
+        {
+            var compPsyche = ___pawn.compPsyche();
+            if (compPsyche! == null)
+            {
+                compPsyche.NullifyCheck();
+            }
+        }
+    }
+}
