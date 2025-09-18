0ee5d162e95b1f3e0368daf028fdc5991d1b01f5
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-09-18 12:55:59 +0900

    Feat: Leave Deeptalk selection weight as is

--- Source/1.6/HarmonyPatches/InteractionWorker_DeepTalk_SelectionWeightPatch.cs
+++ Source/1.6/HarmonyPatches/InteractionWorker_DeepTalk_SelectionWeightPatch.cs
@@ -1,17 +1,17 @@
-﻿using HarmonyLib;
-using RimWorld;
-using Verse;
+﻿// using HarmonyLib;
+// using RimWorld;
+// using Verse;
 
-namespace Maux36.RimPsyche
-{
-    [HarmonyPatch(typeof(InteractionWorker_DeepTalk), nameof(InteractionWorker_DeepTalk.RandomSelectionWeight))]
-    public static class InteractionWorker_DeepTalk_SelectionWeightPatch
-    {
-        [HarmonyPrefix]
-        public static bool RimPsycheOverrideDeepTalk(InteractionWorker_DeepTalk __instance, ref float __result, Pawn initiator, Pawn recipient)
-        {
-            __result = 0f;
-            return false;
-        }
-    }
-}
+// namespace Maux36.RimPsyche
+// {
+//     [HarmonyPatch(typeof(InteractionWorker_DeepTalk), nameof(InteractionWorker_DeepTalk.RandomSelectionWeight))]
+//     public static class InteractionWorker_DeepTalk_SelectionWeightPatch
+//     {
+//         [HarmonyPrefix]
+//         public static bool RimPsycheOverrideDeepTalk(InteractionWorker_DeepTalk __instance, ref float __result, Pawn initiator, Pawn recipient)
+//         {
+//             __result = 0f;
+//             return false;
+//         }
+//     }
+// }
