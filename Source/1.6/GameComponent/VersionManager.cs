f802293d0cd34b6eefc913b41c5914b23139a127
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-11-30 02:40:27 +0900

    Feat: Start managing versions

--- Source/1.6/GameComponent/VersionManager.cs
+++ Source/1.6/GameComponent/VersionManager.cs
@@ -1,27 +1,37 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using Verse;
 
 namespace Maux36.RimPsyche
 {
     public class VersionManager : GameComponent
     {
-        public static string pre_updateVersion = "";
+        public static Version pre_updateVersion = new("0.0.0");
         public static string lastKnownModVersion = "1.0.0";
-        public static bool modVersionUpdated = false;
+        public static HashSet<int> DiscardedPawnThingIDnumber = [];
+
+        public static Version sexuality_variables_implemented = new("1.0.23");
+        public static bool shouldSetupSexualityVariable = false;
 
         public VersionManager(Game game) : base()
         {
-            //if (Rimpsyche.currentVersion != lastKnownModVersion)
-            //{
-            //    modVersionUpdated = true;
-            //    pre_updateVersion = lastKnownModVersion;
-            //    lastKnownModVersion = Rimpsyche.currentVersion;
-            //    Log.Message($"[Rimpsyche] Version updated from {pre_updateVersion} -> {lastKnownModVersion}");
-            //}
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                Log.Message($"[Rimpsyche] Last known version: {lastKnownModVersion} | current version: {Rimpsyche.currentVersion}");
+                if (Rimpsyche.currentVersion != lastKnownModVersion)
+                {
+                    Log.Message($"[Rimpsyche] Version updated {lastKnownModVersion} -> {Rimpsyche.currentVersion}");
+                    pre_updateVersion = new Version(lastKnownModVersion);
+                    lastKnownModVersion = Rimpsyche.currentVersion;
+                    shouldSetupSexualityVariable = (pre_updateVersion < sexuality_variables_implemented);
+                }
+                DiscardedPawnThingIDnumber.Clear();
+            }
             Scribe_Values.Look(ref lastKnownModVersion, "lastKnownMyModVersion", "0.0.0");
         }
     }
