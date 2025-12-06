1598c3471cc536d517316e5a4b2a346cd0080080
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-06 21:07:14 +0900

    Feat: Move sexuality settings to Sexuality module

--- Source/1.6/Rimpsyche.cs
+++ Source/1.6/Rimpsyche.cs
@@ -36,7 +36,7 @@ namespace Maux36.RimPsyche
             return "RimpsycheSettingCategory".Translate();
         }
         private static Vector2 scrollPosition = new Vector2(0f, 0f);
-        private static float totalContentHeight = ModsConfig.BiotechActive ? 770f : 720f;
+        private static float totalContentHeight = 400f;
         private const float ScrollBarWidthMargin = 18f;
         public override void DoSettingsWindowContents(Rect inRect)
         {
@@ -57,57 +57,8 @@ namespace Maux36.RimPsyche
             listing_Standard.Gap(6f);
             listing_Standard.CheckboxLabeled("RimpsycheShowFacetGraph".Translate(), ref RimpsycheSettings.showFacetGraph, "RimpsycheShowFacetGraphTooltip".Translate());
 
-            if(SexualityModuleLoaded)
-            {
-                listing_Standard.Gap(12f);
-                listing_Standard.Label("RimpsycheKinseyDistributionSettings".Translate());
-                Rect sliderArea = listing_Standard.GetRect(200f);
-                DrawKinseyDistributionSliders(sliderArea);
-                listing_Standard.Gap(12f);
-                listing_Standard.CheckboxLabeled("RimpsycheRomanceAttemptGenderDiff".Translate(), ref RimpsycheSettings.romanceAttemptGenderDiff, "RimpsycheRomanceAttemptGenderDiffTooltip".Translate());
-            }
-
             listing_Standard.End();
             Widgets.EndScrollView();
-        }
-        private void DrawKinseyDistributionSliders(Rect rect)
-        {
-            float total = 0f;
-            foreach (var value in RimpsycheSettings.KinseyDistributionSetting)
-            {
-                total += value;
-            }
-            const int numSliders = 7;
-            float spacing = 5f;
-            float sliderWidth = 50f;
-            float sliderHeight = rect.height - 100f; // Leave space for labels
-            string pcText;
-
-            for (int i = 0; i < numSliders; i++)
-            {
-                float x = rect.x + i * (sliderWidth + spacing);
-                Widgets.Label(new Rect(x, rect.y, sliderWidth, 20f), i.ToString());
-                Rect sliderRect = new Rect(x, rect.y+20f, sliderWidth, sliderHeight);
-                RimpsycheSettings.KinseyDistributionSetting[i] = (int)(GUI.VerticalSlider(
-                    sliderRect,
-                    RimpsycheSettings.KinseyDistributionSetting[i],
-                    100f,
-                    0f
-                ));
-
-                Rect valLabelRect = new Rect(x, sliderRect.yMax, sliderWidth, 20f);
-                Widgets.Label(valLabelRect, RimpsycheSettings.KinseyDistributionSetting[i].ToString());
-                Rect pcLabelRect = new Rect(x, valLabelRect.yMax, sliderWidth, 20f);
-                if (total == 0f) pcText = "(" + (1f / 7f).ToStringPercent() + ")";
-                else pcText = "(" + (RimpsycheSettings.KinseyDistributionSetting[i] / total).ToStringPercent() + ")";
-                Widgets.Label(pcLabelRect, pcText);
-            }
-            Rect buttonRect = new Rect(rect.x, rect.yMax-30f, 300f, 30f);
-            if(Widgets.ButtonText(buttonRect, "RimpsycheKinseyDefaultSetting".Translate()))
-            {
-                //Slightly adjusted from https://yougov.co.uk/society/articles/12999-half-young-not-heterosexual
-                RimpsycheSettings.KinseyDistributionSetting = RimpsycheSettings.DefaultDistribution;
-            }
         }
     }
 }
