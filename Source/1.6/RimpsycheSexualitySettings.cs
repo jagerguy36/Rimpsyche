2749662664f28f2be23f70735730f74b87178502
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-12-12 01:42:36 +0900

    Feat: Preference setting default false

--- Source/1.6/RimpsycheSexualitySettings.cs
+++ Source/1.6/RimpsycheSexualitySettings.cs
@@ -7,7 +7,7 @@ namespace Maux36.RimPsyche
     {
         public static readonly List<int> DefaultDistribution = [62, 11, 10, 6, 5, 3, 3];
         public static List<int> KinseyDistributionSetting = [.. DefaultDistribution];
-        public static bool usePreferenceSystem = true;
+        public static bool usePreferenceSystem = false;
         public static bool romanceAttemptGenderDiff = true;
         public static float minRelAttraction = 0.7f;
 
@@ -22,7 +22,7 @@ namespace Maux36.RimPsyche
 
             Scribe_Collections.Look(ref KinseyDistributionSetting, "KinseyDistributionSetting", LookMode.Value);
             Scribe_Values.Look(ref usePreferenceSystem, "usePreferenceSystem", true);
-            Scribe_Values.Look(ref romanceAttemptGenderDiff, "romanceAttemptGenderDiff", true);
+            Scribe_Values.Look(ref romanceAttemptGenderDiff, "romanceAttemptGenderDiff", false);
             Scribe_Values.Look(ref minRelAttraction, "minRelAttraction", 0.7f);
 
         }
