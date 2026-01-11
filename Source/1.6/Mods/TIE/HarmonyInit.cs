e17cdf58ccbef45cae07623c857ddc85b3e15a04
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2026-01-11 12:59:12 +0900

    Feat: TIE init

---
+++ Source/1.6/Mods/TIE/HarmonyInit.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace Maux36.RimPsyche.Intimacy.TIE
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            var harmony = new Harmony("Harmony_RimpsycheTIE");
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Log.Message($"[Rimpsyche] Talking Isn't Everything patched");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"[Rimpsyche] Talking Isn't Everything patch failed: {e}");
+            }
+        }
+    }
+}
